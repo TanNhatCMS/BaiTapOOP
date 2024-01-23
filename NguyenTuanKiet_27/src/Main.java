9d592af90e1a776f4734cc0e9d1e453f944103a2
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-01-23 15:43:37 +0700

    Update Main.java

--- NguyenTuanKiet_27/src/Main.java
+++ NguyenTuanKiet_27/src/Main.java
@@ -1,3 +1,5 @@
+import utils.HangHoaException;
+
 import java.util.Scanner;
 
 public class Main {
@@ -5,7 +7,29 @@ public class Main {
         QuanLyHangHoa quanLy = new QuanLyHangHoa();
         Scanner scanner = new Scanner(System.in);
 
+        int choice;
+        do {
+            System.out.println("===== MENU =====");
+            System.out.println("1. Thêm hàng hóa");
+            System.out.println("2. In danh sách hàng hóa");
+            System.out.println("0. Thoát");
+            System.out.println("================");
 
-        quanLy.inDanhSachHangHoa();
+            System.out.println("Nhập lựa chọn: ");
+            choice = scanner.nextInt();
+                switch (choice) {
+                    case 1:
+                        quanLy.themHangHoa();
+                        break;
+                    case 2:
+                        quanLy.inDanhSachHangHoa();
+                        break;
+                    case 0:
+                        System.out.println("Kết thúc chương trình.");
+                        break;
+                    default:
+                        System.out.println("Lựa chọn không hợp lệ.");
+                }
+        } while (choice != 0);
     }
 }
