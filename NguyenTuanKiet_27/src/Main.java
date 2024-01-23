73dcb31c293c672d0aaf0c28dd29559877f6375e
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-01-23 15:05:28 +0700

    Create Main.java

---
+++ NguyenTuanKiet_27/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        QuanLyHangHoa quanLy = new QuanLyHangHoa();
+        Scanner scanner = new Scanner(System.in);
+
+
+        quanLy.inDanhSachHangHoa();
+    }
+}
