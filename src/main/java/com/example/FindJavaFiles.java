d5a3536a9df88498198a829295366ce39b2ceee4
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-01-23 23:11:22 +0700

    Update FindJavaFiles.java

--- src/main/java/com/example/FindJavaFiles.java
+++ src/main/java/com/example/FindJavaFiles.java
@@ -1,27 +1,39 @@
-package com.example;
-
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FindJavaFiles {
+    private static final String OUTPUT_FILE = "javaFiles.txt";
+
     public static void main(String[] args) {
-        File projectDir = new File(System.getProperty("user.dir"));
-        findAndPrintJavaFiles(projectDir);
+        File projectDir = new File(args[0]);
+        findAndWriteJavaFiles(projectDir);
     }
 
-    private static void findAndPrintJavaFiles(File directory) {
-        if (directory.isDirectory()) {
-            File[] files = directory.listFiles((dir, name) -> name.endsWith(".java") && !name.equals("MainMethodTest.java"));
-            if (files != null) {
-                for (File file : files) {
-                    System.out.println(file.getAbsolutePath());
-                }
+    private static void findAndWriteJavaFiles(File directory) {
+        try (FileWriter writer = new FileWriter(OUTPUT_FILE)) {
+            if (directory.isDirectory()) {
+                findAndWriteJavaFiles(directory, writer);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-            File[] subdirectories = directory.listFiles(File::isDirectory);
-            if (subdirectories != null) {
-                for (File subdirectory : subdirectories) {
-                    findAndPrintJavaFiles(subdirectory);
-                }
+    private static void findAndWriteJavaFiles(File directory, FileWriter writer) throws IOException {
+        File[] files = directory.listFiles((dir, name) -> name.endsWith(".java") && !name.equals("MainMethodTest.java"));
+        if (files != null) {
+            for (File file : files) {
+                String path = file.getAbsolutePath();
+                System.out.println("Found Java file: " + path);
+                writer.write(path + System.lineSeparator());
+            }
+        }
+
+        File[] subdirectories = directory.listFiles(File::isDirectory);
+        if (subdirectories != null) {
+            for (File subdirectory : subdirectories) {
+                findAndWriteJavaFiles(subdirectory, writer);
             }
         }
     }
