ba1ac0c4bd228c4825d064549f373692276bb741
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-01-23 21:50:02 +0700

    init

---
+++ src/main/java/com/example/FindJavaFiles.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import java.io.File;
+
+public class FindJavaFiles {
+    public static void main(String[] args) {
+        File projectDir = new File(System.getProperty("user.dir"));
+        findAndPrintJavaFiles(projectDir);
+    }
+
+    private static void findAndPrintJavaFiles(File directory) {
+        if (directory.isDirectory()) {
+            File[] files = directory.listFiles((dir, name) -> name.endsWith(".java"));
+            if (files != null) {
+                for (File file : files) {
+                    System.out.println("Found Java file: " + file.getAbsolutePath());
+                }
+            }
+
+            File[] subdirectories = directory.listFiles(File::isDirectory);
+            if (subdirectories != null) {
+                for (File subdirectory : subdirectories) {
+                    findAndPrintJavaFiles(subdirectory);
+                }
+            }
+        }
+    }
+}
