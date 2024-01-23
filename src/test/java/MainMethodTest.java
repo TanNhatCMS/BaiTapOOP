566078d72867f468983f9e95a2b3046c265b2518
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-01-23 22:30:29 +0700

    Update MainMethodTest.java

--- src/test/java/MainMethodTest.java
+++ src/test/java/MainMethodTest.java
@@ -1,30 +1,57 @@
 import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.*;
+import java.util.stream.Stream;
 
-public class MainMethodTest {
+class MainMethodTest {
 
     @Test
-    public void testMainMethods() {
-        // Thay đổi gói và classNames tùy thuộc vào cấu trúc dự án của bạn
-        String packageName = "com.example";
-        String[] classNames = {"Class1", "Class2"};
-
-        for (String className : classNames) {
-            try {
-                Class<?> clazz = Class.forName(packageName + "." + className);
-                Method mainMethod = clazz.getMethod("main", String[].class);
-
-                // Kiểm tra nếu phương thức main được tìm thấy
-                if (mainMethod != null) {
-                    System.out.println("Found main method in class: " + className);
-                    // Thực hiện các bước kiểm thử khác tùy thuộc vào yêu cầu của bạn
-                    // ...
-                } else {
-                    System.out.println("No main method found in class: " + className);
-                }
-            } catch (ClassNotFoundException | NoSuchMethodException e) {
-                System.out.println("Error: " + e.getMessage());
-            }
+    void testMainMethods() {
+        try {
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            String currentPackage = getClass().getPackageName();
+            Path basePath = getPathToClasses(classLoader, currentPackage);
+            scanAndInvokeMainMethods(basePath, classLoader);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    private Path getPathToClasses(ClassLoader classLoader, String packageName) throws URISyntaxException {
+        String packagePath = packageName.replace('.', '/');
+        URL resource = classLoader.getResource(packagePath);
+        return Paths.get(resource.toURI());
+    }
+
+    private void scanAndInvokeMainMethods(Path basePath, ClassLoader classLoader) throws IOException {
+        try (Stream<Path> paths = Files.walk(basePath)) {
+            paths.filter(Files::isRegularFile)
+                    .filter(p -> p.toString().endsWith(".class"))
+                    .forEach(p -> invokeMainMethod(p, classLoader));
+        }
+    }
+
+    private void invokeMainMethod(Path classFile, ClassLoader classLoader) {
+        String className = getClassName(classFile);
+        try {
+            Class<?> loadedClass = classLoader.loadClass(className);
+            Method mainMethod = loadedClass.getMethod("main", String[].class);
+            if (mainMethod != null) {
+                System.out.println("Running main method for class: " + className);
+                mainMethod.invoke(null, (Object) new String[]{});
+            }
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            System.out.println("Error running main method for class " + className + ": " + e.getMessage());
+        }
+    }
+
+    private String getClassName(Path classFile) {
+        String fileName = classFile.getFileName().toString();
+        return fileName.substring(0, fileName.length() - ".class".length()).replace(File.separatorChar, '.');
     }
 }
